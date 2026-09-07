efad7a3aa26c868d3d3ea1e7b657261ce423f923
Aniruddha Roy <ar.roy564@gmail.com>
2026-09-07 19:38:42 +0600

    added kernel panic

--- kernel.c
+++ kernel.c
@@ -34,6 +34,9 @@ void putchar(char ch) {
 void kernel_main(void) {
     memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
+    PANIC("booted!");
+    printf("unreachable here!\n");
+
     char* str = "\nhello world\n";
 
     for (int i = 0; str[i] != '\0'; i++) {
--- kernel.h
+++ kernel.h
@@ -4,3 +4,13 @@ struct sbiret {
     long error;
     long value;
 };
+
+/**
+ * if this was made an function, then it would show the file name and line number where PANIC is defined, not where it's called
+ */
+#define PANIC(fmt, ...)                                                       \
+    do {                                                                      \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+        while (1) {                                                           \
+        }                                                                     \
+    } while (0)
